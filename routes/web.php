0c19b77fb3f62278b4894b34d53df7b7ffa733b3
SKILL573 <a9101021249@gmail.com>
2017-10-17 08:56:52 +0800

    限制參數

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,15 @@ Routes::get('student/{student_no}/score/{subject}',function($student_no,$subject
 
 Routes::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號:".$student_no."的".((is_null($subject))"所有科目":$subject)."成績";
-}):
+});
+
+Routes::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
+})->where(['student_no'=>'s[0-9]{10}']);
+
+Routes::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+    return "學號:".$student_no."的".((is_null($subject))"所有科目":$subject)."成績";
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+
+
     

d499a344e5dcfd080bddf03c8565f8526899a663
M511-51\admin <a9101021249@gmail.com>
2017-10-20 19:02:47 +0800

    練習三

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -20,12 +20,16 @@
 	</nav>
 	<div style="padding-top: 70px;"></div>
 	<div class="container">
+		@extends('layouts.master')
+		@section('title','詳細資料')
+		@section('content')
+
 		<div class="page-header">
 			<h1>詳細資料</h1>
 		</div>
 		<div class="row">
 			<p>
-				學號：123456
+				學號：{{$student_no}}
 			</p>
 			<p>
 				姓名：小明
@@ -33,17 +37,21 @@
 			<p>
 				電話：0912345678
 			</p>
+			@if(is_null($subject)||$subject=='chinese')
 			<p>
 				國文：60
 			</p>
-			<p>
+				@if(is_null($subject)||$subject=='english')
+				<p>
 				英文：60
 			</p>
-			<p>
+					@if(is_null($subject)||$subject=='math')
+					<p>
 				數學：60
 			</p>
 			@endif
 		</div>
+			@stop
 	</div>
 </body>
 </html>

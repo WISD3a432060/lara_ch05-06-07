8f9e82dbf94b3984fd9c7770bac78ee1362b224a
SKILL573 <a9101021249@gmail.com>
2017-11-24 11:10:45 +0800

    註解ROUTE

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,25 @@ Route::get('cool','cool\TestconTroller@index');
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
 });
-
-
+*/
+/*
 Route::get('student/{student_no}/score',function($student_no){
     return "學號:".$student_no."的所有成績";
 });
-
-
+*/
+/*
 Route::get('student/{student_no}/score/{subject}',function($student_no,$subject){
     return "學號:".$student_no."的".$subject."成績";
 });
-
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
-
+*/
+/*
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
 })->where(['student_no'=>'s[0-9]{10}']);
@@ -41,8 +41,8 @@ Route::get('student/{student_no}',function($student_no){
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
-
-
+*/
+/*
 Route::pattern('student_no'=>'s[0-9]{10}');
 Route::group(['prefix'=>'student'],function()
 {
@@ -53,7 +53,7 @@ Route::group(['prefix'=>'student'],function()
         return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
     }) ->where(['subject'=>'(chinese|english|math)']);
 });
-
+*/
 Route::pattern('student_no'=>'s[0-9]{10}');
 Route::group(['prefix'=>'student'],function()
 {
